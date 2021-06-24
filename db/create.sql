576ce984ea45503dd5952e73ca79de435b43208b
zely20@mail.ru <Civik1984>
2021-06-24 21:47:08 +0300

    add reference and table users ti create.sql

--- db/create.sql
+++ db/create.sql
@@ -2,5 +2,13 @@ CREATE TABLE IF NOT EXISTS items (
     id serial PRIMARY KEY,
     description text,
     created timestamp,
-    is_done bool
+    is_done bool,
+    user_id int not null references users(id)
+);
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id       serial PRIMARY KEY,
+    name     text,
+    password text
 );
